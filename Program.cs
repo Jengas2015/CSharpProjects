e29347ac82e6c9c013b1052764e8a27ae616e4fe
Jengas2015 <arejang@gmail.com>
2024-04-19 17:24:28 -0500

    adds C# Caclulator app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int firstNum;
+int secondNum;
+string command;
+
+Console.WriteLine("This is a test calculator!");
+Console.WriteLine("Please enter your first number");
+
+firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Okay! Your first number is {firstNum}");
+Console.WriteLine("Please enter your second number");
+
+
+secondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Okay! Your second number is {secondNum}");
+Console.WriteLine("What would you like to do with these two numbers? You can use commands like 'add', 'subtract', 'divide', or 'multiply'.");
+
+command = Console.ReadLine();
+
+if (command == "add") {
+    Console.WriteLine($"The sum of your two numbers is {firstNum + secondNum}");
+} else if (command =="subtract") {
+    Console.WriteLine($"The difference between  your two numbers is {firstNum - secondNum}");
+} else if (command == "multiply") {
+    Console.WriteLine($"The product of your two numbers is {firstNum * secondNum}");
+} else if (command == "divide") {
+    float quotient = (float)firstNum / secondNum;
+    Console.WriteLine ($"The quotient between your two numbers is {quotient}");
+} else {
+    Console.WriteLine ($"Sorry! I don't recognize that command!");
+}
